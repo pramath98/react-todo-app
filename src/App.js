17664e23f5bfcdfa040e52c6b22e036f3786e432
pramath98 <pramath98@gmail.com>
2024-02-07 10:45:57 +0530

    landing page created

--- src/App.js
+++ src/App.js
@@ -5,13 +5,15 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
 import Welcome from './components/Welcome';
 import Login from './components/Login';
+import LandingPage from './landingPage';
 
 function App() {
   return (
     <BrowserRouter>
         <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/welcome' element={<Welcome/>}/>
+        <Route path='/' element={<LandingPage/>}/>
+        <Route path='/home' element={<Home/>}/>
+        <Route path='/signup' element={<Welcome/>}/>
         <Route path='/login' element={<Login/>}/>
         </Routes>
       </BrowserRouter>
