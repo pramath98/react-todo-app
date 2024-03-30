f9b0d17e94eee2686074b5d48d8092a408573f06
pramath98 <pramath98@gmail.com>
2024-03-30 23:27:28 +0530

    Header created and styles changed

--- src/App.js
+++ src/App.js
@@ -7,11 +7,12 @@ import Home from './components/Home';
 import Welcome from './components/Welcome';
 import Login from './components/Login';
 import LandingPage from './landingPage';
-
+import Header from './components/Header';
 
 function App() {
   return (
     <BrowserRouter>
+        <Header/>
         <Routes>
         <Route path='/' element={<LandingPage/>}/>
         <Route path='/home' element={<Home/>}/>
