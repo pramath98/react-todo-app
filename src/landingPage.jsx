810bd57825f943900e102ea433a626e2f8d35911
pramath98 <pramath98@gmail.com>
2024-06-22 16:28:26 +0530

    landing page UI added

--- src/landingPage.jsx
+++ src/landingPage.jsx
@@ -8,6 +8,19 @@ export default function LandingPage() {
                     <span className='hero-subtext'>Your Personal</span>
                     <span className='hero-text'>Productivity Companion</span>
                 </div>
+                <div className='hero-todo-vector'>
+                    <div className='hero-todo-vector-container'>
+                        <div className='hero-todo-vector-container-header'>
+                            <span className='hero-todo-vector-container-header-text'>Get things done!</span>
+                            <span className='hero-todo-vector-container-header-line' />
+                        </div>
+                        <div className='hero-todo-vector-container-tasks'>
+                            <span>Learn Mathematics</span>
+                            <span>Go to Gym</span>
+                            <span>Bring snacks for the party</span>
+                        </div>
+                    </div>
+                </div>
             </div>
         </>
     )
