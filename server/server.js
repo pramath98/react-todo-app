ef70971ad9117e4d938945e4608d0fb44ee00dd6
pramath98 <pramath98@gmail.com>
2024-10-31 10:19:05 +0530

    making server run 24/7

--- server/server.js
+++ server/server.js
@@ -12,11 +12,11 @@ app.use(cors({
 app.use(express.json());
 app.use('/',recordRouter);
 // get driver connection
-if(process.env.NODE_ENV==='development'){
+// if(process.env.NODE_ENV==='development'){
   app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
   });
-}
+// }
 const dbo = require("./db/conn");
 dbo.connectToServer(function (err) {
   if (err) console.error(err);
